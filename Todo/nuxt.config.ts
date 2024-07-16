0183213e705f53d950a2b75974cd07e0bdc612d9
Meenatchisundharam M <121151601+meenatchisundharam@users.noreply.github.com>
2024-07-16 19:18:08 +0530

    add fontawesomecdn

--- Todo/nuxt.config.ts
+++ Todo/nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        }
+      ]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
-})
+});
